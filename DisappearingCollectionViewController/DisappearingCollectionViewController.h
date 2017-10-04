5b19c5a876a0248e04f641ef6e442ee321a250c8
Miles Hollingsworth <mhmiles@me.com>
2017-10-03 22:08:25 -0700

    Initial Commit

---
+++ DisappearingCollectionViewController/DisappearingCollectionViewController.h
@@ -0,0 +1,19 @@
+//
+//  DisappearingCollectionViewController.h
+//  DisappearingCollectionViewController
+//
+//  Created by Miles Hollingsworth on 10/3/17.
+//  Copyright © 2017 Miles Hollingsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DisappearingCollectionViewController.
+FOUNDATION_EXPORT double DisappearingCollectionViewControllerVersionNumber;
+
+//! Project version string for DisappearingCollectionViewController.
+FOUNDATION_EXPORT const unsigned char DisappearingCollectionViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DisappearingCollectionViewController/PublicHeader.h>
+
+
